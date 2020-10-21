8cc467a1ff356a4e89aebc4ef664bbd0d4a3fdf4
nbahire <n.bahire31@gmail.com>
2020-10-21 12:44:02 +0200

    mis en place de bootstrap via npm et sass

--- assets/app.js
+++ assets/app.js
@@ -6,9 +6,13 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
+import $ from 'jquery';
+import 'bootstrap';
 
-console.log('Hello Webpack Encore! Edit me in assets/app.js');
+$('.custom-file-input').on('change', function (e) {
+    let inputFile = e.currentTarget;
+    $(inputFile).parent().find('.custom-file-label').html(inputFile.files[0].name);
+})
