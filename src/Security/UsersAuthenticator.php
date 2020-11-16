8bc3a6dd42280e70b147fd4a6c95bba279ecaa79
nbahire <n.bahire31@gmail.com>
2020-11-17 00:45:13 +0100

    Modification de la route de connexion

--- src/Security/UsersAuthenticator.php
+++ src/Security/UsersAuthenticator.php
@@ -90,12 +90,17 @@ class UsersAuthenticator extends AbstractFormLoginAuthenticator implements Passw
         return $credentials['password'];
     }
 
-    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $providerKey )
+    public function onAuthenticationSuccess(Request $request,TokenInterface $token, string $providerKey )
     {
         if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
             return new RedirectResponse($targetPath);
         }
-        return new RedirectResponse($this->urlGenerator->generate('app_users'));
+if($token->getRoleNames()[0] === 'ROLE_ADMIN'){
+
+    return new RedirectResponse($this->urlGenerator->generate('app_admin_'));
+    
+};      
+return new RedirectResponse($this->urlGenerator->generate('app_users'));
         //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
